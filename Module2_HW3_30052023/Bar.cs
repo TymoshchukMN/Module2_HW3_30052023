e872c17ed6962663ecc49969c01d0aab5685a332
Tymoshchuk Maksym <MTimoshchukmn.ua@gmail.com>
2023-05-31 15:14:43 +0300

    added classe Milcholate and bar/Porous

---
+++ Module2_HW3_30052023/Bar.cs
@@ -0,0 +1,15 @@
+﻿// Author: Tymoshchuk Maksym
+// Created On : 31/05/2023
+// Last Modified On :
+// Description: bar Chocolate
+// Project: Module2_HW3_26052023
+
+namespace Module2_HW3_30052023
+{
+    /// <summary>
+    /// Bar chocolate.
+    /// </summary>
+    public class Bar : MilkChocolate
+    {
+    }
+}
---
+++ Module2_HW3_30052023/MilkChocolate.cs
@@ -0,0 +1,17 @@
+﻿// Author: Tymoshchuk Maksym
+// Created On : 30/05/2023
+// Last Modified On :
+// Description: MilkChocolate
+// Project: Module2_HW3_26052023
+
+namespace Module2_HW3_30052023
+{
+    using System;
+
+    /// <summary>
+    /// MilkChocolate.
+    /// </summary>
+    public class MilkChocolate : Chocolate
+    {
+    }
+}
