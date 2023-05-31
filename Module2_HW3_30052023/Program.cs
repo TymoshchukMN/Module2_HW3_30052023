e7a869968b78c669d348e63c680014f853b5932e
Tymoshchuk Maksym <MTimoshchukmn.ua@gmail.com>
2023-05-31 10:26:02 +0300

    added style crope

---
+++ Module2_HW3_30052023/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2_HW3_30052023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
